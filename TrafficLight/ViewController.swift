c2a1d51dfc4f8a03a6db11d0f8a24df21fadf58d
meowfield <meowfield@yandex.ru>
2024-06-19 19:53:34 +0200

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Данис Гаязов on 19.6.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
