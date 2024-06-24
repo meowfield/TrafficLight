7933a0672d23d4ece9e10f5d02dda0b233ee1628
meowfield <meowfield@yandex.ru>
2024-06-24 15:01:57 +0200

    Fixed layouts and implemented working logic for TrafficLight app
    - Set up traffic light UI elements (red, yellow, green)
    - Added button to cycle through traffic light states
    - Implemented state change logic for traffic lights (red -> yellow -> green -> red)

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let lightIsOffState = 0.3
+    private let lightIsOnState = 1.0
+    private var currentLight: TrafficLight = .green
+    enum TrafficLight {
+    case red, yellow, green
+    }
+    
+    @IBOutlet var redTrafficLight: UIView!
+    @IBOutlet var yellowTrafficLight: UIView!
+    @IBOutlet var greenTrafficLight: UIView!
+    @IBOutlet var actionButton: UIButton!
+        
+    @IBAction func actionButtonDidTapped(_ sender: UIButton) {
+        actionButton.setTitle("Next", for: .normal)
+        changeTrafficLightState()
+    }
+    
+    func changeTrafficLightState() {
+        switch currentLight {
+        case .red:
+            yellowTrafficLight.alpha = lightIsOnState
+            redTrafficLight.alpha = lightIsOffState
+            currentLight = .yellow
+        case .yellow:
+            greenTrafficLight.alpha = lightIsOnState
+            yellowTrafficLight.alpha = lightIsOffState
+            currentLight = .green
+        case .green:
+            redTrafficLight.alpha = lightIsOnState
+            greenTrafficLight.alpha = lightIsOffState
+            currentLight = .red
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        actionButton.setTitle("Start", for: .normal)
+        actionButton.layer.cornerRadius = 10
+        setupTrafficLight(redTrafficLight)
+        setupTrafficLight(yellowTrafficLight)
+        setupTrafficLight(greenTrafficLight)
+
+        func setupTrafficLight(_ light: UIView) {
+            light.layer.cornerRadius = light.frame.size.width / 2
+            light.alpha = lightIsOffState
+        }
     }
-
-
 }
 
